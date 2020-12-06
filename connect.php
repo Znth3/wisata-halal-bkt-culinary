cebce9964a68a9f7739fdf33511443104dbb401a
mnfaiz <48718272+Znth3@users.noreply.github.com>
2020-12-07 06:29:38 +0700

    Update connect.php
    host

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
 	$user = "postgres";
-	$pass = "root";
-	$port = "5434";
+	$pass = "postgrespass";
+	$port = "5432";
 	$dbname = "bkt_tourism";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
